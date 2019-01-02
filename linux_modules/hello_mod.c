032806d9a7b296ac8acb7c35897cc0e494e87f74
Fabian Schneider <F.abianS.chneider@web.de>
2019-01-02 22:35:52 +0100

    add allocating major number

--- linux_modules/hello_mod.c
+++ linux_modules/hello_mod.c
@@ -1,13 +1,27 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
 
-MODULE_AUTHOR("urs.graf@ntb.ch");
-MODULE_DESCRIPTION("Hello world module");
+MODULE_DESCRIPTION("example char device");
 MODULE_LICENSE("GPL");
+
+static int Major;
+dev_t dev_no;
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	int ret;
+	printk(KERN_ALERT "hello_init function\n");
+	printk (" Inside init module\n");
+	ret = alloc_chrdev_region( &dev_no , 0, 1,"chr_arr_dev");
+	if (ret < 0)
+	{
+		printk("Major number allocation is failed\n");
+		return ret;
+	}
+	
+	Major = MAJOR(dev_no);
+	printk (" The major number of device is %d\n", Major);
 	return 0;
 }
 
