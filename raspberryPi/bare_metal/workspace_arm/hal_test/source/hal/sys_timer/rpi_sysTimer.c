6acf643e4eeaa53bd9c54c4c36d0b84a27654d36
fabian <F.abianS.chneider@web.de>
2019-02-04 21:58:13 +0100

    refactoring

--- raspberryPi/bare_metal/workspace_arm/hal_test/source/hal/sys_timer/rpi_sysTimer.c
+++ raspberryPi/bare_metal/workspace_arm/hal_test/source/hal/sys_timer/rpi_sysTimer.c
@@ -16,9 +16,9 @@ volatile hal_sysTimer_regs_t * hal_sysTimer_GetRegs( void )
 
 void hal_sysTimer_WaitMicroSeconds( hal_base_t MicroSeconds )
 {
-	hal_v_base_t ts = hal_sysTimer_regs->counter_lo;
+	hal_v_base_t counterValue = hal_sysTimer_regs->counter_lo;
 
-    while( ( hal_sysTimer_regs->counter_lo - ts ) < MicroSeconds )
+    while( ( hal_sysTimer_regs->counter_lo - counterValue ) < MicroSeconds )
     {
         // nop
     }
